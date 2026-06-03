65ea34dadc9a89b611904dd33baa8bb3aa36748c
srishtiii-17 <psrishti17@gmail.com>
2026-06-03 15:33:00 +0530

    Updated

---
+++ day-3/Q9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int n, i, isPrime=1;
+    printf("enter a number");
+    scanf("%d", &n);
+    if (n<=1){
+        isPrime=0;
+    }
+    else{
+        for (i=2; i<=n/2; i++) {
+            if(n % i==0){
+                isPrime=0;
+                break;
+            }
+        }
+    }
+    if (isPrime){
+        printf("prime no.",n);
+    }
+    else {
+        printf("composite no.", n);
+    }
+    return 0;
+}
