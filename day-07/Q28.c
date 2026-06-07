78d6bf8bca25c81eec90629fa6788b9ce6fe78b9
srishtiii-17 <psrishti17@gmail.com>
2026-06-07 22:57:47 +0530

    updated

---
+++ day-07/Q28.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int reverse = 0;
+void reverseNumber(int n) {
+    if (n == 0)
+        return;
+    reverse = reverse * 10 + (n % 10);
+    reverseNumber(n / 10);
+}
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    reverseNumber(num);
+    printf("Reversed number = %d\n", reverse);
+    return 0;
+}
