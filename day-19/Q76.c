aacb257785bda4fecc6d1458aac9903d405c8e70
srishtiii-17 <psrishti17@gmail.com>
2026-06-19 12:44:14 +0530

    updated

---
+++ day-19/Q76.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int n, i, j, sum = 0;
+    printf("Enter size of square matrix: ");
+    scanf("%d", &n);
+    int a[n][n];
+    printf("Enter matrix:\n");
+    for(i = 0; i < n; i++)
+        for(j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+    for(i = 0; i < n; i++)
+        sum += a[i][i];
+    printf("Diagonal Sum = %d", sum);
+    return 0;
+}
